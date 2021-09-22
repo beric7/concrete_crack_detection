895ed2f8b096f3b03dc2ea6ef14b34b8cc13d3b9
beric7 <beric7@vt.edu>
2021-09-22 11:44:38 -0400

    Update rescale_segmentation.py

--- Pre-processing/rescale_segmentation.py
+++ Pre-processing/rescale_segmentation.py
@@ -6,8 +6,6 @@ Created on Sat Jul  4 12:38:49 2020
 """
 import sys
 
-sys.path.insert(0, 'F://Python/general_utils/')
-
 from image_utils import rescale_mask
 
 # rescale(source_image_folder, destination, dimension):
